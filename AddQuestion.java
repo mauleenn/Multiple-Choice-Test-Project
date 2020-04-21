ecfe5532563246bf01d4e8f5ab0aea62206caa30
Adam Herbst <12887762+AdamHerbst@users.noreply.github.com>
2020-04-21 16:34:06 -0400

    latest Add Question

--- AddQuestion.java
+++ AddQuestion.java
@@ -4,11 +4,19 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
+import conn.PostgresConn;
+
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.awt.event.ActionEvent;
 
 public class AddQuestion {
@@ -49,7 +57,11 @@ public class AddQuestion {
 	/**
 	 * Create the application.
 	 */
+	Connection connection = null;
+	Statement stmt = null;
+	PreparedStatement pstmt = null;
 	public AddQuestion() {
+		connection = PostgresConn.connect();
 		initialize();
 	}
 
@@ -72,20 +84,20 @@ public class AddQuestion {
 		getAddQuestionFrame().getContentPane().add(questionTxtField);
 		questionTxtField.setColumns(10);
 		
-		JLabel answerOneLabel = new JLabel("Answer #1:");
-		answerOneLabel.setBounds(37, 142, 93, 15);
+		JLabel answerOneLabel = new JLabel("Correct Answer:");
+		answerOneLabel.setBounds(12, 142, 118, 15);
 		getAddQuestionFrame().getContentPane().add(answerOneLabel);
 		
-		JLabel answerTwoLabel = new JLabel("Answer #2:");
-		answerTwoLabel.setBounds(37, 199, 93, 15);
+		JLabel answerTwoLabel = new JLabel("Wrong Answer 1:");
+		answerTwoLabel.setBounds(12, 199, 118, 15);
 		getAddQuestionFrame().getContentPane().add(answerTwoLabel);
 		
-		JLabel answerThreeLabel = new JLabel("Answer #3:");
-		answerThreeLabel.setBounds(37, 258, 93, 15);
+		JLabel answerThreeLabel = new JLabel("Wrong answer 2:");
+		answerThreeLabel.setBounds(12, 258, 118, 15);
 		getAddQuestionFrame().getContentPane().add(answerThreeLabel);
 		
-		JLabel answerFourLabel = new JLabel("Answer #4:");
-		answerFourLabel.setBounds(37, 316, 93, 15);
+		JLabel answerFourLabel = new JLabel("Wrong answer 3:");
+		answerFourLabel.setBounds(12, 316, 118, 15);
 		getAddQuestionFrame().getContentPane().add(answerFourLabel);
 		
 		answerOneTxtField = new JTextField();
@@ -108,39 +120,61 @@ public class AddQuestion {
 		answerFourTxtField.setBounds(137, 311, 527, 26);
 		getAddQuestionFrame().getContentPane().add(answerFourTxtField);
 		
-		
-		// Checkboxes that determine if an answer is correct
-		JCheckBox answerOneCheckBox = new JCheckBox();
-		answerOneCheckBox.setBounds(672, 138, 35, 23);
-		getAddQuestionFrame().getContentPane().add(answerOneCheckBox);
-		
-		JCheckBox answerTwoCheckBox = new JCheckBox();
-		answerTwoCheckBox.setBounds(672, 195, 35, 23);
-		getAddQuestionFrame().getContentPane().add(answerTwoCheckBox);
-		
-		JCheckBox answerThreeCheckBox = new JCheckBox();
-		answerThreeCheckBox.setBounds(672, 254, 35, 23);
-		getAddQuestionFrame().getContentPane().add(answerThreeCheckBox);
-		
-		JCheckBox answerFourCheckBox = new JCheckBox();
-		answerFourCheckBox.setBounds(672, 312, 35, 23);
-		getAddQuestionFrame().getContentPane().add(answerFourCheckBox);
-		
 		JButton submitButton = new JButton("Submit");
 		submitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				// When this button is pressed, the question 
 				// should be added to the database and we should
 				// see a "success" message.
+				add();
 			}
 		});
 		submitButton.setForeground(Color.BLACK);
 		submitButton.setBackground(Color.LIGHT_GRAY);
 		submitButton.setBounds(351, 386, 117, 25);
 		getAddQuestionFrame().getContentPane().add(submitButton);
+
+	}
+	public void add() {
+
+		try {
+			String Question = questionTxtField.getText();
+			String answer1 = answerOneTxtField.getText();
+			String answer2 = answerTwoTxtField.getText();
+			String answer3 = answerThreeTxtField.getText();
+			String answer4 = answerFourTxtField.getText();
+			
+
+				
+	        String sql = "insert into questions(question)values(?)";  
+	        PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);  
+	        ps.setString(1, Question);  
+	        ps.execute();  
+
+
+	        ResultSet rs = ps.getGeneratedKeys();
+	        int newId = 0;
+	        if (rs.next()) {
+	        	 newId = rs.getInt(1);
+	        }
+	            String answerQuery = "insert into answers(id, correct_answer, wrong_answer1, wrong_answer2, wrong_answer3)values((?),?,?,?,?)";  
+		        PreparedStatement answerInsert = connection.prepareStatement(answerQuery);  
+		        answerInsert.setInt(1, newId);  
+		        answerInsert.setString(2, answer1);
+		        answerInsert.setString(3, answer2);
+		        answerInsert.setString(4, answer3);
+		        answerInsert.setString(5, answer4);
+		        answerInsert.executeUpdate();  
+		        JOptionPane.showMessageDialog(addQuestionFrame, "Added question and answers");
+	        
+		        ps.close();
+		        rs.close();
 		
-		
-		
-		
+
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(null, ex);
+		}
 	}
 }
+
+	
