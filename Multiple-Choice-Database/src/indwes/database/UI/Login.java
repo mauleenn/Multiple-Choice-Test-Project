3eba9041a2929b15498c15d3cb4b09e58793dd63
mauleenn <60015202+mauleenn@users.noreply.github.com>
2020-04-23 20:36:07 -0500

    Updated Login Page
    Login page now has working functionality.

--- Multiple-Choice-Database/src/indwes/database/UI/Login.java
+++ Multiple-Choice-Database/src/indwes/database/UI/Login.java
@@ -1,6 +1,9 @@
 package indwes.database.UI;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
@@ -8,14 +11,25 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+
+import conn.PostgresConn;
+import net.proteanit.sql.DbUtils;
+
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.awt.event.ActionEvent;
+import java.awt.SystemColor;
 
 public class Login {
 
 	private JFrame loginPageFrame;
 	private JTextField passwordTxtField;
+	private JTextField IDTxtField;
 
 	// Getter and Setter for accessing to this window
 	public JFrame getLoginPageFrame() {
@@ -24,6 +38,7 @@ public class Login {
 
 	public void setLoginPageFrame(JFrame loginPageFrame) {
 		this.loginPageFrame = loginPageFrame;
+		loginPageFrame.getContentPane().setBackground(SystemColor.activeCaption);
 	}
 
 	/**
@@ -45,8 +60,13 @@ public class Login {
 	/**
 	 * Create the application.
 	 */
+	Connection connection = null;
+	Statement stmt = null;
+	PreparedStatement pstmt = null;
+
 	public Login() {
 		initialize();
+		connection = PostgresConn.connect(); // Connecting to the database
 	}
 
 	/**
@@ -55,67 +75,128 @@ public class Login {
 	private void initialize() {
 		setLoginPageFrame(new JFrame());
 		getLoginPageFrame().setTitle("Login");
-		getLoginPageFrame().setBounds(100, 100, 519, 323);
+		getLoginPageFrame().setBounds(100, 100, 553, 426);
 		getLoginPageFrame().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getLoginPageFrame().getContentPane().setLayout(null);
-		
-		
 
+		// Username label & text field
 		JLabel usernameLabel = new JLabel("Username:");
-		usernameLabel.setBounds(80, 65, 90, 15);
+		usernameLabel.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		usernameLabel.setForeground(Color.WHITE);
+		usernameLabel.setBounds(125, 101, 90, 15);
 		getLoginPageFrame().getContentPane().add(usernameLabel);
 
+		JTextField usernameTxtField = new JTextField();
+		usernameTxtField.setColumns(10);
+		usernameTxtField.setBounds(222, 97, 166, 23);
+		getLoginPageFrame().getContentPane().add(usernameTxtField);
+
+		// ID label & text field
+		JLabel IDLabel = new JLabel("ID:");
+		IDLabel.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		IDLabel.setForeground(Color.WHITE);
+		IDLabel.setBounds(125, 61, 42, 15);
+		getLoginPageFrame().getContentPane().add(IDLabel);
+
+		IDTxtField = new JTextField();
+		IDTxtField.setBounds(222, 57, 166, 23);
+		getLoginPageFrame().getContentPane().add(IDTxtField);
+		IDTxtField.setColumns(10);
+
+		// Password label & text field
 		JLabel passwordLabel = new JLabel("Password:");
-		passwordLabel.setBounds(83, 113, 87, 15);
+		passwordLabel.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		passwordLabel.setForeground(Color.WHITE);
+		passwordLabel.setBounds(125, 146, 87, 15);
 		getLoginPageFrame().getContentPane().add(passwordLabel);
 
-		JTextField usernameTxtField = new JTextField();
-		usernameTxtField.setBounds(212, 57, 192, 31);
-		getLoginPageFrame().getContentPane().add(usernameTxtField);
-		usernameTxtField.setColumns(10);
-		
 		passwordTxtField = new JPasswordField();
 		((JPasswordField) passwordTxtField).setEchoChar('#');
-		passwordTxtField.setBounds(212, 120, 192, 31);
+		passwordTxtField.setBounds(222, 142, 167, 23);
 		getLoginPageFrame().getContentPane().add(passwordTxtField);
 		passwordTxtField.setColumns(10);
-		
+
+		// Student checkbox
 		JCheckBox studentChkBox = new JCheckBox("Student");
-		studentChkBox.setBounds(70, 161, 90, 23);
+		studentChkBox.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		studentChkBox.setBackground(SystemColor.activeCaption);
+		studentChkBox.setForeground(SystemColor.text);
+		studentChkBox.setBounds(138, 182, 71, 23);
 		getLoginPageFrame().getContentPane().add(studentChkBox);
 		studentChkBox.setSelected(false);
-		
+
+		// Teacher checkbox
 		JCheckBox teacherChkBox = new JCheckBox("Teacher");
-		teacherChkBox.setBounds(164, 157, 129, 31);
+		teacherChkBox.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		teacherChkBox.setBackground(SystemColor.activeCaption);
+		teacherChkBox.setForeground(Color.WHITE);
+		teacherChkBox.setBounds(217, 178, 78, 31);
 		getLoginPageFrame().getContentPane().add(teacherChkBox);
 		teacherChkBox.setSelected(false);
-		
+
 		JButton btnLogin = new JButton("Login");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
-				// If the user checks the "teacher" checkbox, 
-				// they will be taken to the Teacher View.
-				if(teacherChkBox.isSelected()) {
+				String ID = IDTxtField.getText();
+				int id = Integer.parseInt(ID);
+				String username = usernameTxtField.getText();
+				String password = passwordTxtField.getText();
+
+				if (username.contentEquals("")) {
+					JOptionPane.showMessageDialog(null, "Please enter a username!");
+				} else if (password.equals("")) {
+					JOptionPane.showMessageDialog(null, "Please enter a password!");
+				}
+				else if(!studentChkBox.isSelected() && !studentChkBox.isSelected()) {
+					JOptionPane.showMessageDialog(null, "You MUST ");
+				}
+				// If the user checks the 'teacher' checkbox,
+				// they are lead to the teacher view
+				if (teacherChkBox.isSelected()) {
+					try {
+						Connection conn = PostgresConn.connect();
+						PreparedStatement preparedstm = conn.prepareStatement("SELECT * FROM teacher_login WHERE id=? AND username=? AND password=?");
+
+						preparedstm.setInt(1, id);
+						preparedstm.setString(5, username);
+						preparedstm.setString(2, password);
+
+						preparedstm.executeQuery();
+
+					} catch (Exception e) {
+						System.out.println(e);
+					}
+					JOptionPane.showMessageDialog(null, "Success!");
 					TeacherView window = new TeacherView();
 					window.getTeacherViewFrame().setVisible(true);
 				}
-				
-				// If the user checks the "student" checkbox, 
-				// they will be taken to the Student View.
-				else if (studentChkBox.isSelected()) {
+				// If the user checks the 'student' checkbox,
+				// they are lead to the student view
+				if (studentChkBox.isSelected()) {
+					try {
+
+						Connection conn = PostgresConn.connect();
+						PreparedStatement preparedstmA = conn.prepareStatement("SELECT * FROM user_login WHERE id=? AND username=? AND password=?");
+
+						preparedstmA.setInt(1, id);
+						preparedstmA.setString(5, username);
+						preparedstmA.setString(2, password);
+
+						preparedstmA.executeQuery();
+
+					} catch (Exception e) {
+						System.out.println(e);
+					}
+					JOptionPane.showMessageDialog(null, "Success!");
 					StudentView window = new StudentView();
 					window.getStudentViewFrame().setVisible(true);
 				}
-				else if (usernameTxtField.getText().equals("") || passwordTxtField.getText().equals("")) {
-					JOptionPane.showMessageDialog(null, "You must enter a username or password");
-				}
-				
+
 			}
 		});
-		btnLogin.setBounds(108, 214, 117, 25);
+		btnLogin.setBounds(138, 218, 117, 25);
 		getLoginPageFrame().getContentPane().add(btnLogin);
-		
+
 		// If the user does not have an account,
 		// they will be taken to the "Sign Up"
 		// page.
@@ -126,9 +207,27 @@ public class Login {
 				window.getCreateAccountFrame().setVisible(true);
 			}
 		});
-		btnSignUp.setBounds(278, 214, 117, 25);
+		btnSignUp.setBounds(279, 218, 117, 25);
 		getLoginPageFrame().getContentPane().add(btnSignUp);
-		
-		
+
+	}
+
+	public void login() {
+
+		try {
+
+			String id = (IDTxtField.getText());
+			int ID = Integer.parseInt(id);
+			String password = passwordTxtField.getText();
+
+			Connection conn = PostgresConn.connect();
+			PreparedStatement preparedstm = conn
+					.prepareStatement("SELECT * FROM public.user_login WHERE id=? AND password=?");
+
+			ResultSet rs = preparedstm.executeQuery();
+			conn.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 }
